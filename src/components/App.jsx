6fda7d7b24e1d0e0c984630f7eb665f366ac182c
Maxim Noritsyn <maximnoritsyn@gmail.com>
2024-04-07 12:51:45 +0300

    state to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,36 @@
 import {Feedback} from './feedback/feedback';
+import { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-        backgroundColor: '#f0f0f0'
-      }}
-    >
-      <Feedback />
-    </div>
-  );
+export class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  onLeaveFeedback = (e) => {
+      const feedbackType = e.target.dataset.type;
+      this.setState((prevState) => ({
+          [feedbackType]: prevState[feedbackType] + 1
+      }));
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+          backgroundColor: '#f0f0f0'
+        }}
+      >
+        <Feedback options={this.state} onLeaveFeedback={this.onLeaveFeedback} />
+      </div>
+    );
+  }
 };
